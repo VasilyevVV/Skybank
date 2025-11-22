c3139c16e5a49ce3d3d0c3186e8aaa22541dc89d
Vasilyev Vyacheslav <vasilev.vyacheslav15@gmail.com>
2025-11-22 20:39:54 +0700

    fixed progress on homework 9.1

---
+++ src/masks.py
@@ -0,0 +1,9 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Функция принимает на вход номер карты и возвращает ее в формате XXXX XX** **** XXXX"""
+    card_number_str = str(card_number)
+    return f"{card_number_str[0:4]} {card_number_str[4:6]}** **** {card_number_str[-4::]}"
+
+
+def get_mask_account(account_number: int) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску в формате **XXXX, где X — цифра номера"""
+    return "**" + str(account_number)[-4::]
