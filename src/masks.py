7bdb9d117e2f847d0499064987ace2235e485e6e
Vasilyev Vyacheslav <vasilev.vyacheslav15@gmail.com>
2025-11-23 15:56:58 +0700

    final changes before submitting homework 9.2

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,9 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: int | str) -> str:
     """Функция принимает на вход номер карты и возвращает ее в формате XXXX XX** **** XXXX"""
-    card_number_str = str(card_number)
+    card_number_str = str(card_number).replace(" ", "")
     return f"{card_number_str[0:4]} {card_number_str[4:6]}** **** {card_number_str[-4::]}"
 
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: int | str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску в формате **XXXX, где X — цифра номера"""
     return "**" + str(account_number)[-4::]
--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,29 @@
-from .masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_info: str) -> str:
-    """Функция обрабатки информацию о картах и счетах. Возвращает строку с с замаскированным номером"""
-    account_list = account_info.split(" ")
-    masked_info = ""
-    account_info_mask = ""
-    if len(account_list[-1]) == 16:
-        masked_info = get_mask_card_number(int(account_list[-1]))
-    elif len(account_list[-1]) == 20:
-        masked_info = get_mask_account(int(account_list[-1]))
-    account_info_mask = f"{" ".join(account_list[:-1])} {masked_info}"
+    """Функция обрабатывает информацию о картах и счетах. Возвращает строку с замаскированным номером"""
+    account_inf_list = account_info.split(" ")
+    masked_number = ""
+    # Если на входе строка с типом и номером карты, то номер - последний элемент - состоит из 16 символов
+    if len(account_inf_list[-1]) == 16:
+        masked_number = get_mask_card_number(account_inf_list[-1])
+    # Если на входе строка с номером счёта - 20 цифр
+    elif len(account_inf_list[-1]) == 20:
+        masked_number = get_mask_account(account_inf_list[-1])
+    # Вывод: все элементы входной строки без последнего (ноvера счёта или карты)
+    # и номер (кары или счёта)  с маской
+    account_info_mask = f"{" ".join(account_inf_list[:-1])} {masked_number}"
     return account_info_mask
 
 
-info = "Maestro 1596837868705199"
-print(mask_account_card(info))
+def get_date(date_string: str) -> str:
+    """Функция принимает на вход строку с датой в формате "2025-07-16T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("16.07.2025"
+    """
+    # Определение разделитель в подстроке ГГГГ-ММ-ДД
+    separator = date_string[4]
+    # Деление на подстроки - создание списка и разворачивание его с разделителем "." - точка
+    date_parts = date_string[0:10].split(separator)
+    date_dmy = ".".join(date_parts[::-1])
+    return date_dmy
