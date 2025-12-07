45e34c1b0eb4946a47a86dddfad36d02fbb0992d
Vasilyev Vyacheslav <vasilev.vyacheslav15@gmail.com>
2025-12-07 21:45:48 +0700

    add test function for module widget

--- src/widget.py
+++ src/widget.py
@@ -3,27 +3,37 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(account_info: str) -> str:
     """Функция обрабатывает информацию о картах и счетах. Возвращает строку с замаскированным номером"""
-    account_inf_list = account_info.split(" ")
+    if account_info == "": # Если на вход подана пустая строка
+        raise ValueError("Отсутствует информация о карте / счёте")
+    account_info_list = account_info.split(" ")
     masked_number = ""
     # Если на входе строка с типом и номером карты, то номер - последний элемент - состоит из 16 символов
-    if len(account_inf_list[-1]) == 16:
-        masked_number = get_mask_card_number(account_inf_list[-1])
+    if len(account_info_list[-1]) == 16:
+        masked_number = get_mask_card_number(account_info_list[-1])
     # Если на входе строка с номером счёта - 20 цифр
-    elif len(account_inf_list[-1]) == 20:
-        masked_number = get_mask_account(account_inf_list[-1])
+    elif len(account_info_list[-1]) == 20:
+        masked_number = get_mask_account(account_info_list[-1])
     # Вывод: все элементы входной строки без последнего (ноvера счёта или карты)
     # и номер (кары или счёта)  с маской
-    account_info_mask = f"{" ".join(account_inf_list[:-1])} {masked_number}"
+    elif len(account_info_list[-1]) < 16 or len(account_info_list[-1]) > 20:
+        raise ValueError("Некорректный номер карты или счета")
+    account_info_mask = f"{" ".join(account_info_list[:-1])} {masked_number}"
     return account_info_mask
 
+
+# acc_info = "Счет 35383033474447895560"
+# print(mask_account_card(acc_info))
 
 def get_date(date_string: str) -> str:
     """Функция принимает на вход строку с датой в формате "2025-07-16T02:26:18.671407"
     и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("16.07.2025"
     """
     # Определение разделитель в подстроке ГГГГ-ММ-ДД
-    separator = date_string[4]
-    # Деление на подстроки - создание списка и разворачивание его с разделителем "." - точка
-    date_parts = date_string[0:10].split(separator)
-    date_dmy = ".".join(date_parts[::-1])
-    return date_dmy
+    if date_string == "":
+        raise ValueError("Отсутствует иинформация о дате-времени")
+    else:
+        separator = date_string[4]
+        # Деление на подстроки - создание списка и разворачивание его с разделителем "." - точка
+        date_parts = date_string[0:10].split(separator)
+        date_dmy = ".".join(date_parts[::-1])
+        return date_dmy
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,1 +1,58 @@
+import pytest
 from src.widget import mask_account_card, get_date
+
+# Тест корректности обработки информации о карте / счёте
+@pytest.mark.parametrize("card_acc_info, expected", [("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+                                                     ("Visa Platinum 8990922113665228", "Visa Platinum 8990 92** **** 5228"),
+                                                     ("МИР 2574446287251099", "МИР 2574 44** **** 1099"),
+                                                     ("Счет 64686473678894779579", "Счет **9579"),
+                                                     ("Счет 35383033474447895560", "Счет **5560")
+                                                     ])
+def test_mask_account_card(card_acc_info, expected):
+    assert mask_account_card(card_acc_info) == expected
+
+
+# Тест на не цифровые символы в номере карты / счёта
+@pytest.mark.parametrize("card_acc_info", [("Maestro 1,56837868705199"),
+                                           ("Visa Gold 59994142284263uf"),
+                                           ("Счет 7V6541084301358743+5"),
+                                           ("Счет ADCD30334744478955**")])
+def test_invalid_character(card_acc_info):
+    with pytest.raises(ValueError) as exc_info:
+        mask_account_card(card_acc_info)
+        assert str(exc_info.value) == "Номер карты должен состоять только из цифр" or str(exc_info.value) == "Некорректный номер счета. Номер может содержать только цифры"
+
+
+# Тест на некорректную длину (формат) номера карты - 16 цифр, или счёта - 20 цифр.
+@pytest.mark.parametrize("acc_info", [("Maestro 1568378"),
+                                      ("Visa Gold 5999414228426325789614"),
+                                      ("Счет 78471"),
+                                      ("Счет 30334744478955787879745102574")])
+def test_incorrect_acc_info(acc_info):
+    with pytest.raises(ValueError) as exc_info:
+        mask_account_card(acc_info)
+    assert str(exc_info.value) == "Некорректный номер карты или счета"
+
+# Тест на поустую строку на входе функции mask_account_card
+def test_empty_account_info():
+    with pytest.raises(ValueError) as exc_info:
+        mask_account_card("")
+        assert str(exc_info.value) == "Отсутствует информация о карте / счёте"
+
+
+@pytest.mark.parametrize("date_str, expected", [("2025-07-08T02:26:18.671407", "08.07.2025"),
+                                                ("2025/02/28T05:16:25.641954", "28.02.2025"),
+                                                ("2024.08.31T12:16:25.1534", "31.08.2024")
+                                                ])
+# Тест корректной работы функции get_date
+def test_get_date(date_str, expected):
+    assert get_date(date_str) == expected
+
+
+# Тест на поустую строку на входе функции get_date
+def test_epty_date():
+    with pytest.raises(ValueError) as exc_info:
+        get_date("")
+        assert str(exc_info.value) == "Отсутствует информация о карте / счёте"
+
+
