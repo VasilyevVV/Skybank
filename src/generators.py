825627eac126d3227c5d27d69c3e8a9213da97db
Vasilyev Vyacheslav <vasilev.vyacheslav15@gmail.com>
2025-12-20 15:42:37 +0700

    add module generators and test_generators

---
+++ src/generators.py
@@ -0,0 +1,59 @@
+from typing import Iterator
+
+
+def filter_by_currency(transactions: list, currency: str = "RUB") -> Iterator[dict]:
+    """
+    Функция принимает на вход список словарей, представляющих транзакции.
+    Возвращает итератор, поочередно выдающий транзакции,
+    где валюта операции соответствует заданной (по умолчанию "USD")
+    """
+    for one_transaction in transactions:
+        # Отбираем только те транзакции, в которых валюта операции соответствует заданной (например, "RUB").
+        if one_transaction.get("operationAmount", {}).get("currency", {}).get("code", "unknown") == currency:
+            yield one_transaction
+
+
+def transaction_descriptions(transactions: list) -> Iterator[str]:
+    """
+    Функция - генератор, принимает список словарей с транзакциями
+    и возвращает описание каждой операции по очереди.
+    """
+    for transaction in transactions:
+        # Если описание транзакции отсутствует - нет ключа "description", то описание = "n/a"
+        tx_description = transaction.get("description", "n/a")
+        if tx_description != "n/a":
+            yield tx_description
+
+
+def card_number_generator(start: str | int, stop: str | int) -> Iterator[str]:
+    """
+    Функция - генератор, выдает номера банковских карт в формате "XXXX XXXX XXXX XXXX", где X — цифра номера карты.
+    Принимает начальное и конечное значения для генерации диапазона номеров.
+    Генерирует номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
+    """
+    # Проверка корректности начального значения start, если это не целое число
+    if not isinstance(start, int):
+        # если строка - это целое число, иначе генерация исключения ValueError
+        if str(start).isdigit():
+            start_value = int(start)
+        else:
+            raise ValueError("Некорректное начальное значение")
+    else:
+        start_value = start
+    # Проверка корректности конечного значения stop, если это не целое число
+    if not isinstance(stop, int):
+        # если строка - это целое число, иначе генерация исключения ValueError
+        if str(stop).isdigit():
+            stop_value = int(stop)
+        else:
+            raise ValueError("Некорректное конечное значение")
+    else:
+        stop_value = stop
+    # Сравнение start_value < stop_value
+    if start_value <= stop_value:
+        # Если диапазон коррекктный - генерация номерров карт в формате "XXXX XXXX XXXX XXXX"
+        for i in range(start_value, stop_value + 1):
+            # card_number = str(i).zfill(16)
+            card_number = f"{i:016d}"
+            formatted_card_num = f"{card_number[0:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
+            yield formatted_card_num
---
+++ tests/test_generators.py
@@ -0,0 +1,92 @@
+import pytest
+
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
+
+
+# Тест функции filter_by_currency (по умолчанию currency="RUB")
+def test_filter_by_currency(transaction_list, rub_expected, usd_expected):
+    rub_transaction = filter_by_currency(transaction_list)
+    usd_transactions = filter_by_currency(transaction_list, "USD")
+    assert list(rub_transaction) == rub_expected
+    assert list(usd_transactions) == usd_expected
+
+
+# Тест функции filter_by_currency, когда указанная валюта отсутствует в списке транзакций
+def test_missing_currency(transaction_list):
+    with pytest.raises(StopIteration):
+        tx = filter_by_currency([], "CNY")
+        next(tx)
+
+
+# Tecn функции filter_by_currenc на пустой список транзакций на входе
+def test_empty_filter_by_currency():
+    with pytest.raises(StopIteration):
+        tx = filter_by_currency([], "USD")
+        next(tx)
+
+
+# Тест функции transaction_descriptions
+def test_transaction_descriptions_2(transaction_list, trans_descript_list):
+    descriptions = transaction_descriptions(transaction_list)
+    assert [desc for desc in descriptions] == trans_descript_list
+
+
+# Teст функции transaction_descriptions, если на входе пустой список
+def test_empty_transaction_descriptions(transaction_list):
+    with pytest.raises(StopIteration):
+        descr = transaction_descriptions([])
+        next(descr)
+
+
+# Тест функции card_number_generator - генератора номеров карт
+@pytest.mark.parametrize(
+    "start, stop, expected_output",
+    [
+        (
+            "000070021086001",
+            "000070021086005",
+            [
+                "0000 0700 2108 6001",
+                "0000 0700 2108 6002",
+                "0000 0700 2108 6003",
+                "0000 0700 2108 6004",
+                "0000 0700 2108 6005",
+            ],
+        ),
+        (
+            1004,
+            1008,
+            [
+                "0000 0000 0000 1004",
+                "0000 0000 0000 1005",
+                "0000 0000 0000 1006",
+                "0000 0000 0000 1007",
+                "0000 0000 0000 1008",
+            ],
+        ),
+    ],
+)
+def test_card_number_generator(start, stop, expected_output):
+    result = card_number_generator(start, stop)
+    assert [card_number for card_number in result] == expected_output
+
+
+# Тест функции card_number_generator с некорректно заданным диапазоном: stop меньше start
+def test_incorrect_range():
+    with pytest.raises(StopIteration):
+        numbers = card_number_generator(5, "1")
+        next(numbers)
+
+
+# Тест функции card_number_generator с некорректно заданными start или stop (нечисловыми значениями)
+@pytest.mark.parametrize(
+    "start, stop, exp_out",
+    [
+        ("R108", 6005, ValueError),
+        ("1008", "DF", ValueError),
+    ],
+)
+def test_invalid_start_stop_values(start, stop, exp_out):
+    with pytest.raises(exp_out):
+        numbers = card_number_generator(start, stop)
+        next(numbers)
