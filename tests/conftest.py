0a8c97d4ae190bd1c9966e1ce0c203cc06a94a19
Vasilyev Vyacheslav <vasilev.vyacheslav15@gmail.com>
2026-01-09 18:24:23 +0700

    add external_api and tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -255,3 +255,112 @@ def transaction_list_from_json():
             "to": "Счет 41421565395219882431",
         },
     ]
+
+
+# Фикстура - словарь с транзакцией в "RUB"
+@pytest.fixture
+def transaction_dict_rub():
+    return {
+        "id": 45743855,
+        "state": "EXECUTED",
+        "date": "2025-05-02T10:50:00.291521",
+        "operationAmount": {"amount": "10808.80", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589",
+    }
+
+
+# Фикстура - словарь с транзакцией в неизвестной валюте (не в EUR или USD)
+@pytest.fixture
+def transaction_unknown():
+    return {
+        "id": 441945886,
+        "state": "CANCELLED",
+        "date": "2024-07-02T03:20:00.521621",
+        "operationAmount": {"amount": "7.80", "currency": {"name": "abc", "code": "ABC"}},
+        "description": "Перевод",
+        "from": "VISA 1596837868705199",
+        "to": "Счет 64686473678894779589",
+    }
+
+
+# Фикстура с некорректным ключом "operationAmount"
+@pytest.fixture
+def invalid_transaction_1():
+    return {
+        "id": 441975216,
+        "state": "EXECUTED",
+        "date": "2025-10-12T07:20:15.291521",
+        "operation": {"amount": "100.00", "currency": {"name": "руб.", "code": "EUR"}},
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589",
+    }
+
+
+# Фикстура с некорректным ключом "currency"
+@pytest.fixture
+def invalid_transaction_2():
+    return {
+        "id": 441975216,
+        "state": "EXECUTED",
+        "date": "2025-10-12T07:20:15.291521",
+        "operationAmount": {"amount": "10808.80", "curr": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589",
+    }
+
+
+# Фикстура с некорректным ключом "code"
+@pytest.fixture
+def invalid_transaction_3():
+    return {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2025-03-27T11:55:14.915214",
+        "operationAmount": {"amount": "10.00", "currency": {"name": "руб.", "cod": "USD"}},
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589",
+    }
+
+
+# Фикстура с пустым значением валюты - "code" ("")
+@pytest.fixture
+def blank_code_transaction():
+    return {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2024-01-02T05:57:10.152146",
+        "operationAmount": {"amount": "80.80", "currency": {"name": "руб.", "code": ""}},
+        "description": "Перевод со счета на счет",
+        "from": "Visa GOLD 1596837868705199",
+        "to": "Счет 64686473678894779589",
+    }
+
+
+@pytest.fixture
+def unknown_tx_currency():
+    return {
+        "id": 441945886,
+        "state": "CANCELLED",
+        "date": "2024-07-02T03:20:00.521621",
+        "operationAmount": {"amount": "7.80", "currency": {"name": "abc", "code": "ABC"}},
+        "description": "Перевод",
+        "from": "VISA 1596837868705199",
+        "to": "Счет 64686473678894779589",
+    }
+
+
+# Фикстура - результат успешного запроса к сервису конвертации
+@pytest.fixture
+def mock_responce():
+    return {
+        "success": True,
+        "query": {"from": "EUR", "to": "RUB", "amount": 100},
+        "info": {"timestamp": 1767870547, "rate": 93.88745},
+        "date": "2026-01-07",
+        "result": 938874.5,
+    }
---
+++ tests/test_external_api.py
@@ -0,0 +1,68 @@
+import pytest
+from unittest.mock import Mock, patch
+from requests import HTTPError
+from src.external_api import get_transaction_sum, get_exchange_rates, API_KEY
+
+
+# Проверка суммы транзакции в RUB. transaction_dict_rub, transaction_unknown - фикстуры-словари с транзакциями
+def test_transaction_sum(transaction_dict_rub, transaction_unknown):
+    assert get_transaction_sum(transaction_dict_rub) == 10808.80
+    assert get_transaction_sum(transaction_unknown) == 0.0
+
+
+# Тест с некорректными транзакциями (словарями), в том числе с пустым
+def test_invalid_transaction(invalid_transaction_1, invalid_transaction_2, invalid_transaction_3):
+    with pytest.raises(ValueError) as exc_info:
+        get_transaction_sum({})
+    assert str(exc_info.value) == "Ошибка чтения транзакции"
+    # Ошибка в ключе "operationAmount"
+    with pytest.raises(ValueError) as exc_info1:
+        get_transaction_sum(invalid_transaction_1)
+    assert str(exc_info1.value) == "Ошибка чтения транзакции"
+    # Ошибка в ключе "currency"
+    with pytest.raises(ValueError) as exc_info2:
+        get_transaction_sum(invalid_transaction_2)
+    assert str(exc_info2.value) == "Ошибка чтения транзакции"
+    # Ошибка в ключе "code"
+    with pytest.raises(ValueError) as exc_info3:
+        get_transaction_sum(invalid_transaction_3)
+    assert str(exc_info3.value) == "Ошибка чтения транзакции"
+
+
+# Тест на пусттое значение кода валюты - code
+def test_blank_code_transaction(blank_code_transaction):
+    with pytest.raises(ValueError) as exc_info:
+        get_transaction_sum(blank_code_transaction)
+    assert str(exc_info.value) == "Ошибка при указании валюты для конвертирования"
+
+
+# Tecn функции get_exchange_rates при успешном обращении к сервису конвертирования, mock_responce - фикстура
+@patch("requests.get")
+def test_exchange_rates(mock_get, mock_responce):
+    mock_get.return_value.json.return_value = mock_responce
+    mock_get.return_value.status_code = 200
+    assert get_exchange_rates(10000, "EUR") == 938874.5
+    mock_get.assert_called_once_with(
+        "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=EUR&amount=10000", headers={"apikey": API_KEY}
+    )
+
+
+# Тестирование функции get_exchange_rates при неуспешных запросах к сервису конвертирования
+@pytest.mark.parametrize(
+    "response_status_code, response_reason, expected",
+    [
+        (400, "Bad Request", "Ошибка выполнения запроса. Возможная причина: "),
+        (401, "Unauthorized", "Ошибка выполнения запроса. Возможная причина: "),
+        (403, "Forbidden", "Ошибка выполнения запроса. Возможная причина: "),
+        (522, "Server Error", "Ошибка выполнения запроса. Возможная причина: "),
+    ],
+)
+@patch("requests.get")
+def test_bad_request_exchange_rates(mock_get, response_status_code, response_reason, expected):
+    mock_response = Mock()
+    mock_response.status_code = response_status_code
+    mock_response.reason = response_reason
+    mock_get.return_value = mock_response
+    with pytest.raises(HTTPError) as err_info:
+        get_exchange_rates(100, "EUR")
+    assert str(err_info.value) == f"{expected}{response_reason}"
